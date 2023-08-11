c26484c7667392a9ea5f34bc2c1789b08aff2f2e
BaptisS <56864846+BaptisS@users.noreply.github.com>
2023-08-11 15:38:51 +0200

    Update tf_export.sh

--- tf_export.sh
+++ tf_export.sh
@@ -2,4 +2,6 @@ complist=$(oci iam compartment list --all --compartment-id-in-subtree true)
 complistcur=$(echo $complist | jq .data | jq -r '.[] | ."id"')
 mkdir $HOME/resource-discovery
 cd $HOME/resource-discovery
-for compocid in $complistcur;do compname=$(oci iam compartment get --compartment-id $compocid --query 'data."name"' | jq -r) && mkdir $compname && terraform-provider-oci_v5.5.0 -command=export -compartment_id=$compocid -services=core -generate_state -output_path=$compname/ ; done
+for compocid in $complistcur;do compname=$(oci iam compartment get --compartment-id $compocid --query 'data."name"' | jq -r '') && mkdir $compname && terraform-provider-oci_v5.5.0 -command=export -compartment_id=$compocid -services=core -output_path=$compname/ ; done
+cd $compname/
+for compocid in $complistcur;do compname=$(oci iam compartment get --compartment-id $compocid --query 'data."name"' | jq -r '') && cd ../$compname && terraform-provider-oci_v5.5.0 -command=export -compartment_id=$compocid -services=core -generate_state -output_path=./ ; done
